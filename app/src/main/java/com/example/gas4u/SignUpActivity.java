bf8ec2e84277024f56ffa68140b9d4fb1fe45ffa
cat201Project <cat201project@gmail.com>
2022-12-07 15:22:19 +0800

    commit by syasya

--- app/src/main/java/com/example/gas4u/SignUpActivity.java
+++ app/src/main/java/com/example/gas4u/SignUpActivity.java
@@ -55,7 +55,7 @@ public class SignUpActivity extends AppCompatActivity {
             editTextPassword.requestFocus();
         }
         if (txtPhoneNo.isEmpty()){
-            editTextMobileNo.setError("Please Enter Phone No");
+            editTextMobileNo.setError("Please Enter Mobile No");
             editTextMobileNo.requestFocus();
         }
         if (txtEmail.isEmpty()){
