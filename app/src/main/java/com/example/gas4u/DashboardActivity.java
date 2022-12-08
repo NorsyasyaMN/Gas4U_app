9f4872282469d5cb175404b812547f8b3ed8549c
cat201Project <cat201project@gmail.com>
2022-12-08 23:02:30 +0800

    Forth commit by syasya

--- app/src/main/java/com/example/gas4u/DashboardActivity.java
+++ app/src/main/java/com/example/gas4u/DashboardActivity.java
@@ -1,14 +1,28 @@
 package com.example.gas4u;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class DashboardActivity extends AppCompatActivity {
+import com.example.gas4u.databinding.ActivityDashboardBinding;
+import com.google.android.material.navigation.NavigationView;
+
+public class DashboardActivity extends DrawerBaseActivity {
+
+
+    ActivityDashboardBinding activityDashboardBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        activityDashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(activityDashboardBinding.getRoot());
     }
 }
--- app/src/main/java/com/example/gas4u/DrawerBaseActivity.java
+++ app/src/main/java/com/example/gas4u/DrawerBaseActivity.java
@@ -1,14 +1,48 @@
 package com.example.gas4u;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+        import androidx.appcompat.app.ActionBarDrawerToggle;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.widget.Toolbar;
+        import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.FrameLayout;
 
-public class DrawerBaseActivity extends AppCompatActivity {
+        import com.google.android.material.navigation.NavigationView;
+
+public class DrawerBaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer_base);
+    public void setContentView(View view) {
+        drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_drawer_base, null);
+        FrameLayout container = drawerLayout.findViewById(R.id.activityContainer);
+        container.addView(view);
+        super.setContentView(drawerLayout);
+
+        Toolbar toolbar = drawerLayout.findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = drawerLayout.findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_drawer_open, R.string.menu_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    protected void allocateActivityTitle(String titleString){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle(titleString);
+        }
     }
 }
