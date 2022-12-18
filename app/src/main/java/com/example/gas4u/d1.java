dfddfbcdc4cd2b9e9dff81380c432971c024a963
Nliana <liawako@gmail.com>
2022-12-19 00:32:47 +0800

    add java files: d1, d2, d3, d4

---
+++ app/src/main/java/com/example/gas4u/d1.java
@@ -0,0 +1,14 @@
+package com.example.gas4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class d1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d1);
+    }
+}
---
+++ app/src/main/java/com/example/gas4u/d4.java
@@ -0,0 +1,14 @@
+package com.example.gas4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class d4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d4);
+    }
+}
