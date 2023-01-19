be276c295b050c6abbe6608e71aa74fa4b715436
Nliana <liawako@gmail.com>
2023-01-20 00:58:18 +0800

    add AdapterProductUser, AdapterShop, dialog_quantity, ic_add_black, and ic_remove_black

---
+++ app/src/main/java/com/example/gas4u/AdapterShop.java
@@ -0,0 +1,8 @@
+package com.example.gas4u;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterShop extends RecyclerView.Adapter<AdapterShop.HolderShop> {
+
+    private Context
+}
